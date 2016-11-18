b6f1684aa1a69975629d46221d03ad5c68afc7c6
farhanaahmed <farhana.ahmed.fima@gmail.com>
2016-11-18 16:33:04 +0600

    18/11/2016

---
+++ Timus 1409.c
@@ -0,0 +1,21 @@
+/**
+Program name : Timus 1409(I found this problem on timus online judge site...problem no. 1409)
+Solved On :18/11/2016,11:30 pm
+Solved By : Farhana Ahmed Fima
+**/
+
+
+#include<stdio.h>
+int main()
+
+
+{
+    int a,b,c,d,e;
+    scanf("%d %d",&a,&b);
+    c=(a+b)-1;
+    d=c-a;
+    e=c-b;
+    printf("%d %d\n",d,e);
+    return 0;
+
+}
